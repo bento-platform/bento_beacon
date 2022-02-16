0ea1ad54751fa0521440eea2c463e424fb92d64e
Gordon Krieger <gordon.krieger@gmail.com>
2022-02-16 18:16:56 -0500

    return exceptions in beacon format

--- bento_beacon/utils/exceptions.py
+++ bento_beacon/utils/exceptions.py
@@ -1,16 +1,21 @@
-from flask import jsonify
+from ..utils.beacon_response import build_response_meta
 
 
 class APIException(Exception):
-    def __init__(self, message=None, status_code=None, payload=None):
+    def __init__(self, message="Internal Server Error", status_code=None, payload=None):
         super().__init__()
         self.message = message
         self.status_code = status_code
         self.payload = payload
 
     def beacon_exception(self):
-        # TODO: build proper beacon error response 
-        return jsonify({"beacon error message": self.message})
+        return {
+            "meta": build_response_meta(),
+            "error": {
+                "errorCode": 500,
+                "errorMessage": self.message
+            }
+        }
 
 
 class NotImplemented(APIException):
@@ -18,50 +23,3 @@ class NotImplemented(APIException):
         super().__init__()
         self.message = message
         self.status_code = status_code
-
-
-
-
-
-# Beacon error schema
-
-# {
-#   "$schema": "http://json-schema.org/draft-07/schema",
-#   "type": "object",
-#   "description": "An unsuccessful operation.",
-#   "properties": {
-#     "meta": {
-#       "description": "Information about the response that could be relevant for the Beacon client in order to interpret the results.",
-#       "$ref": "./sections/beaconResponseMeta.json"
-#     },
-#     "error": {
-#       "description": "Returning an error.",
-#       "$ref": "../common/beaconCommonComponents.json#/definitions/BeaconError"
-#     }
-#   },
-#   "required": ["meta", "error"],
-#   "additionalProperties": true
-# }
-
-
-
-
-    #     "BeaconError": {
-    #   "description": "Beacon-specific error.",
-    #   "type": "object",
-    #   "required": ["errorCode"],
-    #   "properties": {
-    #     "errorCode": {
-    #       "type": "integer",
-    #       "format": "int32",
-    #       "examples": ["404"],
-    #       "description": "Entry not found"
-    #     },
-    #     "errorMessage": {
-    #       "type": "string",
-    #       "examples": [
-    #         "the provided parameters are incomplete. 'xyz' is missing."
-    #       ]
-    #     }
-    #   }
-    # },
