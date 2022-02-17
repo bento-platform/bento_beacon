9c5b0e3850da5356e46202c1ba915c1b60ecd51b
Gordon Krieger <gordon.krieger@gmail.com>
2022-02-17 18:04:07 -0500

    start biosamples

--- bento_beacon/endpoints/biosamples.py
+++ bento_beacon/endpoints/biosamples.py
@@ -1,7 +1,12 @@
+from json import JSONDecodeError
+import requests
 from flask import Blueprint, jsonify, current_app
-from ..utils.exceptions import NotImplemented
+from ..utils.exceptions import APIException, NotImplemented
+from ..utils.beacon_response import beacon_response
+from ..utils.katsu_queries import query_katsu
 
 biosamples = Blueprint("biosamples", __name__, url_prefix="/api")
+
 
 # TODO
 
@@ -11,7 +16,8 @@ def get_biosamples():
 
 @biosamples.route("/biosamples/<id>", methods=['GET', 'POST'])
 def biosamples_by_id(id):
-    raise NotImplemented()
+    r=get_katsu_biosamples_by_id(id)
+    return r
 
 @biosamples.route("/biosamples/<id>/g_variants", methods=['GET', 'POST'])
 def variants_by_biosample(id):
@@ -24,3 +30,14 @@ def analyses_by_biosample(id):
 @biosamples.route("/biosamples/<id>/runs", methods=['GET', 'POST'])
 def runs_by_biosample(id):
     raise NotImplemented()
+
+
+# katsu GET /biosamples/{id} optional params (all strings):
+# individual, procedure, is_control_sample, description, sampled_tissue, taxonomy, histological_diagnosis, tumor_progression, tumor_grade, extra_properties
+
+def get_katsu_biosamples_by_id(id):
+    katsu_response = query_katsu(current_app.config["KATSU_BIOSAMPLES_ENDPOINT"], id)
+
+    # TODO: error handling, not found handling
+    return beacon_response(katsu_response)
+
