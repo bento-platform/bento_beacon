ca901383fe4b639b382f1a9c20ddd670806ce5ef
Gordon Krieger <gordon.krieger@gmail.com>
2022-03-21 17:26:11 -0400

    formatting

--- bento_beacon/endpoints/biosamples.py
+++ bento_beacon/endpoints/biosamples.py
@@ -15,18 +15,22 @@ biosamples = Blueprint("biosamples", __name__, url_prefix="/api")
 def get_biosamples():
     raise NotImplemented()
 
+
 @biosamples.route("/biosamples/<id>", methods=['GET', 'POST'])
 def biosamples_by_id(id):
-    r=get_katsu_biosamples_by_id(id)
+    r = get_katsu_biosamples_by_id(id)
     return r
+
 
 @biosamples.route("/biosamples/<id>/g_variants", methods=['GET', 'POST'])
 def variants_by_biosample(id):
     raise NotImplemented()
 
+
 @biosamples.route("/biosamples/<id>/analyses", methods=['GET', 'POST'])
 def analyses_by_biosample(id):
     raise NotImplemented()
+
 
 @biosamples.route("/biosamples/<id>/runs", methods=['GET', 'POST'])
 def runs_by_biosample(id):
@@ -37,7 +41,8 @@ def runs_by_biosample(id):
 # individual, procedure, is_control_sample, description, sampled_tissue, taxonomy, histological_diagnosis, tumor_progression, tumor_grade, extra_properties
 
 def get_katsu_biosamples_by_id(id):
-    katsu_response = query_katsu(current_app.config["KATSU_BIOSAMPLES_ENDPOINT"], id)
+    katsu_response = query_katsu(
+        current_app.config["KATSU_BIOSAMPLES_ENDPOINT"], id)
 
     current_app.logger.info(katsu_response)
 
@@ -47,4 +52,3 @@ def get_katsu_biosamples_by_id(id):
 
     mapped_repsonse = katsu_biosample_to_beacon_biosample(katsu_response)
     return beacon_response([mapped_repsonse])
-
