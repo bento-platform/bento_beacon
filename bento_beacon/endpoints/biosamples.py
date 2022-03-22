1c50c0c3c426465db21e79ec79048454a11ba0b1
Gordon Krieger <gordon.krieger@gmail.com>
2022-03-22 17:27:28 -0400

    biosamples endpoint

--- bento_beacon/endpoints/biosamples.py
+++ bento_beacon/endpoints/biosamples.py
@@ -9,17 +9,37 @@ from ..utils.beacon_mappings import katsu_biosample_to_beacon_biosample
 biosamples = Blueprint("biosamples", __name__, url_prefix="/api")
 
 
-# TODO
+# TODO: pass beacon filtering terms as katsu queries 
 
 @biosamples.route("/biosamples", methods=['GET', 'POST'])
 def get_biosamples():
-    raise NotImplemented()
+    katsu_response = query_katsu(
+        current_app.config["KATSU_BIOSAMPLES_ENDPOINT"])
+
+    current_app.logger.info(katsu_response)
+    if katsu_not_found(katsu_response):
+        current_app.logger.info("katsu not found")
+        return beacon_response([])
+
+    # expect response with properties "count", "next", "previous", "results"
+    results = katsu_response.get("results")
+    mapped_results = list(map(katsu_biosample_to_beacon_biosample, results))
+    return beacon_response(mapped_results)
 
 
 @biosamples.route("/biosamples/<id>", methods=['GET', 'POST'])
-def biosamples_by_id(id):
-    r = get_katsu_biosamples_by_id(id)
-    return r
+def get_katsu_biosamples_by_id(id):
+    katsu_response = query_katsu(
+        current_app.config["KATSU_BIOSAMPLES_ENDPOINT"], id)
+
+    current_app.logger.info(katsu_response)
+
+    if katsu_not_found(katsu_response):
+        current_app.logger.info("katsu not found")
+        return beacon_response([])
+
+    mapped_repsonse = katsu_biosample_to_beacon_biosample(katsu_response)
+    return beacon_response([mapped_repsonse])
 
 
 @biosamples.route("/biosamples/<id>/g_variants", methods=['GET', 'POST'])
@@ -35,20 +55,3 @@ def analyses_by_biosample(id):
 @biosamples.route("/biosamples/<id>/runs", methods=['GET', 'POST'])
 def runs_by_biosample(id):
     raise NotImplemented()
-
-
-# katsu GET /biosamples/{id} optional params (all strings):
-# individual, procedure, is_control_sample, description, sampled_tissue, taxonomy, histological_diagnosis, tumor_progression, tumor_grade, extra_properties
-
-def get_katsu_biosamples_by_id(id):
-    katsu_response = query_katsu(
-        current_app.config["KATSU_BIOSAMPLES_ENDPOINT"], id)
-
-    current_app.logger.info(katsu_response)
-
-    if katsu_not_found(katsu_response):
-        current_app.logger.info("katsu not found")
-        return beacon_response([])
-
-    mapped_repsonse = katsu_biosample_to_beacon_biosample(katsu_response)
-    return beacon_response([mapped_repsonse])
