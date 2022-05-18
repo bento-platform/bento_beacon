abbc8b95fd88e3999f05ed106fad9c6e3b3254bd
Gordon Krieger <gordon.krieger@gmail.com>
2022-05-18 12:00:47 -0400

    temp disable biosamples routes

--- bento_beacon/endpoints/biosamples.py
+++ bento_beacon/endpoints/biosamples.py
@@ -3,7 +3,7 @@ import requests
 from flask import Blueprint, jsonify, current_app
 from ..utils.exceptions import APIException, NotImplemented
 from ..utils.beacon_response import beacon_response, katsu_not_found
-from ..utils.katsu_queries import query_katsu
+from ..utils.katsu_utils import query_katsu
 from ..utils.beacon_mappings import katsu_biosample_to_beacon_biosample
 
 biosamples = Blueprint("biosamples", __name__, url_prefix="/api")
@@ -13,34 +13,34 @@ biosamples = Blueprint("biosamples", __name__, url_prefix="/api")
 
 @biosamples.route("/biosamples", methods=['GET', 'POST'])
 def get_biosamples():
-    katsu_response = query_katsu(
-        current_app.config["KATSU_BIOSAMPLES_ENDPOINT"])
+    # katsu_response = query_katsu(
+    #     current_app.config["KATSU_BIOSAMPLES_ENDPOINT"])
 
-    current_app.logger.info(katsu_response)
-    if katsu_not_found(katsu_response):
-        current_app.logger.info("katsu not found")
-        return beacon_response([])
+    # current_app.logger.info(katsu_response)
+    # if katsu_not_found(katsu_response):
+    #     current_app.logger.info("katsu not found")
+    #     return beacon_response([])
 
-    # expect response with properties "count", "next", "previous", "results"
-    results = katsu_response.get("results")
-    mapped_results = list(map(katsu_biosample_to_beacon_biosample, results))
-    return beacon_response(mapped_results)
-
+    # # expect response with properties "count", "next", "previous", "results"
+    # results = katsu_response.get("results")
+    # mapped_results = list(map(katsu_biosample_to_beacon_biosample, results))
+    # return beacon_response(mapped_results)
+    raise NotImplemented()
 
 @biosamples.route("/biosamples/<id>", methods=['GET', 'POST'])
 def get_katsu_biosamples_by_id(id):
-    katsu_response = query_katsu(
-        current_app.config["KATSU_BIOSAMPLES_ENDPOINT"], id)
+    # katsu_response = query_katsu(
+    #     current_app.config["KATSU_BIOSAMPLES_ENDPOINT"], id)
 
-    current_app.logger.info(katsu_response)
+    # current_app.logger.info(katsu_response)
 
-    if katsu_not_found(katsu_response):
-        current_app.logger.info("katsu returned 'not found'")
-        return beacon_response([])
+    # if katsu_not_found(katsu_response):
+    #     current_app.logger.info("katsu returned 'not found'")
+    #     return beacon_response([])
 
-    mapped_repsonse = katsu_biosample_to_beacon_biosample(katsu_response)
-    return beacon_response([mapped_repsonse])
-
+    # mapped_repsonse = katsu_biosample_to_beacon_biosample(katsu_response)
+    # return beacon_response([mapped_repsonse])
+    raise NotImplemented()
 
 @biosamples.route("/biosamples/<id>/g_variants", methods=['GET', 'POST'])
 def variants_by_biosample(id):
