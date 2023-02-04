d7885504cf58498a9d3cb6a5e016611203d65b48
Brennan Brouillette <brennan.brouillette@computationalgenomics.ca>
2023-02-03 23:44:27 -0500

    patch: fixed dev debugpy entrypoint - reload

--- entrypoint.dev.sh
+++ entrypoint.dev.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 cd ./bento_beacon/
-python -m debugpy --wait-for-client --listen 0.0.0.0:5678 -m flask run --no-debugger --no-reload --host=0.0.0.0 --port=5000
+python -m debugpy --wait-for-client --listen 0.0.0.0:5678 -m flask run --no-debugger --host=0.0.0.0 --port=5000
