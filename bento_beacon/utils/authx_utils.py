10d1827ff0dca49fc1fe016023d6878d05edf1f3
Brennan Brouillette <brennan.brouillette@computationalgenomics.ca>
2023-02-04 22:03:36 -0500

    patch: lowercase variable names

--- bento_beacon/utils/authx_utils.py
+++ bento_beacon/utils/authx_utils.py
@@ -5,30 +5,28 @@ import jwt
 
 from flask import request
 from jwt.algorithms import RSAAlgorithm
-m
+
 from .exceptions import AuthXException
 
 class AuthxMiddleware():
     def __init__(self):
         print('middleware initialized')
         # authx poc
-        # TODO: cleanup ---
-        self.OIDC_ISSUER = os.getenv('OIDC_ISSUER', "https://localhost/auth/realms/realm")
-        self.CLIENT_ID = os.getenv('CLIENT_ID', "abc123") 
+        self.oidc_issuer = os.getenv('OIDC_ISSUER', "https://localhost/auth/realms/realm")
+        self.client_id = os.getenv('CLIENT_ID', "abc123") 
 
-        self.OIDC_WELLKNOWN_URL = self.OIDC_ISSUER + "/protocol/openid-connect/certs"
+        self.oidc_wellknown_path = self.oidc_issuer + "/protocol/openid-connect/certs"
 
-        self.OIDC_ALG="RS256" 
+        self.oidc_alg="RS256" 
 
-        r =requests.get(self.OIDC_WELLKNOWN_URL, verify=False)
+        r =requests.get(self.oidc_wellknown_path, verify=False)
         jwks = r.json()
 
         public_keys = jwks["keys"]
-        rsa_key = [x for x in public_keys if x["alg"] == self.OIDC_ALG][0]
+        rsa_key = [x for x in public_keys if x["alg"] == self.oidc_alg][0]
         rsa_key_json_str = json.dumps(rsa_key)
         
         self.public_key = RSAAlgorithm.from_jwk(rsa_key_json_str)
-        # ---
 
     def verify_token(self):
         print("authx checkup")
@@ -42,7 +40,7 @@ class AuthxMiddleware():
 
                 # use idp public_key to validate and parse inbound token
                 try:
-                    payload = jwt.decode(token_str, self.public_key, algorithms=[self.OIDC_ALG], audience="account")
+                    payload = jwt.decode(token_str, self.public_key, algorithms=[self.oidc_alg], audience="account")
                     header = jwt.get_unverified_header(token_str)
                 except jwt.exceptions.ExpiredSignatureError:
                     raise AuthXException('Expired access_token!')
@@ -53,7 +51,7 @@ class AuthxMiddleware():
                 # print(json.dumps(payload, indent=4, separators=(',', ': ')))
             
                 # TODO: parse out relevant claims/data
-                roles = payload["resource_access"][self.CLIENT_ID]["roles"]
+                roles = payload["resource_access"][self.client_id]["roles"]
                 print(roles)
 
             else:
