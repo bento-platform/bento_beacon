8aeaf30cf4de36ed25cdee5d8702182eea5bcc59
David Lougheed <david.lougheed@gmail.com>
2023-03-20 12:57:34 -0400

    fix: no wait-for-client in dev entrypoint

--- run.dev.bash
+++ run.dev.bash
@@ -15,7 +15,7 @@ if [[ -z "${INTERNAL_PORT}" ]]; then
   INTERNAL_PORT=5000
 fi
 
-python -m debugpy --wait-for-client --listen 0.0.0.0:5678 -m flask run \
+python -m debugpy --listen 0.0.0.0:5678 -m flask run \
   --no-debugger \
   --no-reload \
   --host=0.0.0.0 \
