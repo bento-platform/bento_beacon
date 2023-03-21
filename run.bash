ecead29794411402881b659365107351670046ba
David Lougheed <david.lougheed@gmail.com>
2023-03-21 13:27:50 -0400

    lint: cleaner run.bash

--- run.bash
+++ run.bash
@@ -2,12 +2,12 @@
 
 cd /beacon || exit
 
-if [[ -z "${INTERNAL_PORT}" ]]; then
-  # Set default internal port to 5000
-  INTERNAL_PORT=5000
-fi
+export FLASK_APP='bento_beacon.app:app'
 
-python -m gunicorn bento_beacon.app:app \
+# Set default internal port to 5000
+: "${INTERNAL_PORT:=5000}"
+
+python -m gunicorn "${FLASK_APP}" \
   -w 1 \
   --threads $(( 2 * $(nproc --all) + 1)) \
   -b "0.0.0.0:${INTERNAL_PORT}"
