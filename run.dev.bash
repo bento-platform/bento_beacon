8d6821d6d3bb2dbde4ab8beb5693ec5deb07488d
David Lougheed <david.lougheed@gmail.com>
2023-03-22 10:37:33 -0400

    comment [noci]

--- run.dev.bash
+++ run.dev.bash
@@ -12,7 +12,7 @@ export FLASK_APP='bento_beacon.app:app'
 # Set default internal port to 5000
 : "${INTERNAL_PORT:=5000}"
 
-# Set internal debug port, falling back to debugpy default
+# Set internal debug port, falling back to default in a Bento deployment
 : "${DEBUGGER_PORT:=5683}"
 
 python -m debugpy --listen "0.0.0.0:${DEBUGGER_PORT}" -m flask run \
