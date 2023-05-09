27e2d9d14240d55c664c45f9bb58d13863d73769
Gordon Krieger <gordon.krieger@gmail.com>
2023-05-09 19:50:02 +0000

    refactor /individuals

--- bento_beacon/endpoints/individuals.py
+++ bento_beacon/endpoints/individuals.py
@@ -1,9 +1,8 @@
 from flask import Blueprint
-from functools import reduce
 from ..utils.beacon_request import query_parameters_from_request
 from ..utils.beacon_response import beacon_response, add_info_to_response
-from ..utils.katsu_utils import katsu_filters_and_sample_ids_query, katsu_filters_query, katsu_total_individuals_count, search_from_config
-from ..utils.gohan_utils import query_gohan
+from ..utils.katsu_utils import katsu_filters_and_sample_ids_query, katsu_total_individuals_count
+from ..utils.search import biosample_id_search
 
 individuals = Blueprint("individuals", __name__)
 
@@ -12,62 +11,26 @@ individuals = Blueprint("individuals", __name__)
 def get_individuals():
     variants_query, phenopacket_filters, experiment_filters, config_filters = query_parameters_from_request()
 
-    # ----------------------------------------------------------
-    #  collect biosample ids from variant and experiment search
-    # ----------------------------------------------------------
-
-    results_biosample_ids = {}
-
     # if no query, return total count of individuals
     if not (variants_query or phenopacket_filters or experiment_filters or config_filters):
         add_info_to_response("no query found, returning total count")
         total_count = katsu_total_individuals_count()
         return beacon_response({"count": total_count})
 
-    if variants_query:
-        variant_sample_ids = query_gohan(variants_query, "count", ids_only=True)
-        if not variant_sample_ids:
-            return beacon_response({"count": 0, "results": []})
-        results_biosample_ids["variant_sample_ids"] = variant_sample_ids
-    
-    if experiment_filters:
-        experiment_sample_ids = katsu_filters_query(experiment_filters, "experiment", get_biosample_ids=True)
-        if not experiment_sample_ids:
-            return beacon_response({"count": 0, "results": []})
-        results_biosample_ids["experiment_sample_ids"] = experiment_sample_ids
+    # ------------------------------------------------------------------
+    #  collect biosample ids from variant, experiment and config search
+    # ------------------------------------------------------------------
 
-    # compute intersection of everything in "results" dict
-    # dict is either empty or has dict entries that are non-empty
-    if results_biosample_ids:
-        # ornate syntax but avoids a lot of separate edge cases
-        sample_ids = list(reduce(set.intersection, (set(ids) for ids in results_biosample_ids.values())))
-    else:
-        sample_ids = []
+    sample_ids = biosample_id_search(
+        variants_query=variants_query, experiment_filters=experiment_filters, config_filters=config_filters)
 
-    # -----------------------------------------------------------
-    #  collect individual ids from phenopacket and config search
-    # -----------------------------------------------------------
-
-    # an alternative here is to return biosample ids from config search instead of individual ids,
-    # and then move the code to the block above. But this would mean implicitly hardcoding the
-    # linked field set in katsu, which we probably don't want
-
-    results_individual_ids = {}
+    # -----------------
+    #  get individuals
+    # -----------------
 
     # get all individuals from phenopacket search,
     # but limit results to people with matching sample ids from searches above
-    phenopacket_ids = katsu_filters_and_sample_ids_query(phenopacket_filters, "phenopacket", sample_ids)
-    if phenopacket_ids:
-        results_individual_ids["phenopacket_ids"] = phenopacket_ids
-
-    if config_filters:
-        config_ids = search_from_config(config_filters)
-        results_individual_ids["config_ids"] = config_ids
-        
-    if results_individual_ids:
-        individual_ids = list(reduce(set.intersection, (set(ids) for ids in results_individual_ids.values())))
-    else:
-        individual_ids = []
+    individual_ids = katsu_filters_and_sample_ids_query(phenopacket_filters, "phenopacket", sample_ids)
 
     return beacon_response({"count": len(individual_ids), "results": individual_ids})
 
