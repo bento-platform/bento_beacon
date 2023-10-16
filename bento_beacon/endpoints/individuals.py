413478a347fd95e0384349892094d9fb4c39fdc3
Gordon Krieger <gordon.krieger@gmail.com>
2023-10-16 14:25:11 +0000

    add auth to individuals

--- bento_beacon/endpoints/individuals.py
+++ bento_beacon/endpoints/individuals.py
@@ -1,6 +1,6 @@
 from flask import Blueprint
 from functools import reduce
-from ..authz import authz_middleware
+from ..authz.middleware import PERMISSION_DOWNLOAD_DATA, PERMISSION_QUERY_DATA, check_permissions
 from ..utils.beacon_request import (
     query_parameters_from_request,
     summary_stats_requested,
@@ -10,21 +10,26 @@ from ..utils.beacon_response import (
     add_info_to_response,
     add_stats_to_response,
     add_overview_stats_to_response,
-    zero_count_response
+    zero_count_response,
+    beacon_full_response
 )
 from ..utils.katsu_utils import (
     katsu_filters_and_sample_ids_query,
     katsu_total_individuals_count,
-    search_from_config
+    search_from_config,
+    phenopackets_for_ids
 )
 from ..utils.search import biosample_id_search
+from ..utils.handover_utils import handover_for_ids
 
 individuals = Blueprint("individuals", __name__)
 
 
 @individuals.route("/individuals", methods=['GET', 'POST'])
-@authz_middleware.deco_public_endpoint  # TODO: authz - for now. eventually, return more depending on permissions
 def get_individuals():
+
+    full_response = check_permissions([PERMISSION_QUERY_DATA])
+
     variants_query, phenopacket_filters, experiment_filters, config_filters = query_parameters_from_request()
 
     no_query = not (variants_query or phenopacket_filters or experiment_filters or config_filters)
@@ -32,6 +37,7 @@ def get_individuals():
     config_search_only = config_filters and not (variants_query or phenopacket_filters or experiment_filters)
 
     # return total count of individuals if no query
+    # TODO: return default granularity rather than count (default could be bool rather than count)
     if no_query:
         add_info_to_response("no query found, returning total count")
         total_count = katsu_total_individuals_count()
@@ -77,7 +83,42 @@ def get_individuals():
     if summary_stats_requested():
         add_stats_to_response(individual_ids)
 
+    if full_response:
+        return individuals_full_response(individual_ids)
+    
+    # TODO: configurable default response rather than hardcoded counts
     return beacon_response({"count": len(individual_ids), "results": individual_ids})
+
+
+# TODO: pagination (ideally after katsu search gets paginated)
+def individuals_full_response(ids):
+
+    # temp
+    # if len(ids) > 100:
+    #     return {"message": "too many ids for full response"}
+
+    handover_permission = check_permissions([PERMISSION_DOWNLOAD_DATA])
+    handover = handover_for_ids(ids) if handover_permission else {}
+    phenopackets_by_result_set = phenopackets_for_ids(ids).get("results", {})
+    result_ids = list(phenopackets_by_result_set.keys())
+    result_sets = {}
+
+    for r_id in result_ids:
+        results_this_id = phenopackets_by_result_set.get(r_id, {}).get("matches", [])
+        results_count = len(results_this_id)
+        result = {
+            "id": r_id,
+            "setType": "individual",
+            "exists": results_count > 0,
+            "resultsCount": results_count,
+            "results": results_this_id,
+        }
+        handover_this_id = handover.get(r_id, [])
+        if handover_this_id:
+            result["resultsHandovers"] = handover_this_id
+        result_sets[r_id] = result
+
+    return beacon_full_response(result_sets)
 
 
 # -------------------------------------------------------
