d6130da3f0a72056a3225d9198f1b146ea5494a9
Gordon Krieger <gordon.krieger@gmail.com>
2023-10-18 16:03:03 +0000

    another dead import

--- bento_beacon/endpoints/variants.py
+++ bento_beacon/endpoints/variants.py
@@ -4,7 +4,6 @@ from ..utils.beacon_request import query_parameters_from_request
 from ..utils.beacon_response import beacon_response, add_info_to_response, zero_count_response
 from ..utils.gohan_utils import query_gohan, gohan_total_variants_count, gohan_totals_by_sample_id
 from ..utils.search import biosample_id_search
-from ..utils.exceptions import NotImplemented
 
 variants = Blueprint("variants", __name__)
 
