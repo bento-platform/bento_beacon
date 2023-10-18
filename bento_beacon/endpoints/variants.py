5ddb3be952b94d7da621e4cc47c6500a8d8ea2f2
Gordon Krieger <gordon.krieger@gmail.com>
2023-10-18 15:40:46 +0000

    plug new search code into variants

--- bento_beacon/endpoints/variants.py
+++ bento_beacon/endpoints/variants.py
@@ -1,59 +1,55 @@
 from flask import Blueprint
 from ..authz.middleware import authz_middleware
 from ..utils.beacon_request import query_parameters_from_request
-from ..utils.beacon_response import beacon_response, add_info_to_response
+from ..utils.beacon_response import beacon_response, add_info_to_response, zero_count_response
 from ..utils.gohan_utils import query_gohan, gohan_total_variants_count, gohan_totals_by_sample_id
 from ..utils.katsu_utils import katsu_filters_query
+from ..utils.search import biosample_id_search
 from ..utils.exceptions import NotImplemented
 
 variants = Blueprint("variants", __name__)
+
 
 # returns counts only
 @variants.route("/g_variants", methods=['GET', 'POST'])
 @authz_middleware.deco_public_endpoint  # TODO: for now. eventually, return more depending on permissions
 def get_variants():
-    variants_query, phenopacket_filters, experiment_filters = query_parameters_from_request()
+    variants_query, phenopacket_filters, experiment_filters, config_filters = query_parameters_from_request()
+    has_filters = phenopacket_filters or experiment_filters or config_filters
 
     # if no query, return total count of variants
-    if not (variants_query or phenopacket_filters or experiment_filters):
+    if not (variants_query or has_filters):
         add_info_to_response("no query found, returning total count")
         total_count = gohan_total_variants_count()
         return beacon_response({"count": total_count})
 
-    if phenopacket_filters:
-        phenopacket_sample_ids = katsu_filters_query(phenopacket_filters, "phenopacket", get_biosample_ids=True)
-        if not phenopacket_sample_ids:
-            return beacon_response({"count": 0, "results": []})
-
-    if experiment_filters:
-        experiment_sample_ids = katsu_filters_query(experiment_filters, "experiment", get_biosample_ids=True)
-        if not experiment_sample_ids:
-            return beacon_response({"count": 0, "results": []})
-
-    # compute cases for results
-    # some redundant bools for clarity
+    # ----------------------------------------------------------
+    #  collect biosample ids from all filters
+    # ----------------------------------------------------------
     sample_ids = []
-    if phenopacket_filters and experiment_filters:
-        sample_ids = list(set(phenopacket_sample_ids) & set(experiment_sample_ids))
-        # another early return if empty?
-    if phenopacket_filters and not experiment_filters:
-        sample_ids = phenopacket_sample_ids
-    if experiment_filters and not phenopacket_filters:
-        sample_ids = experiment_sample_ids
 
-    # workaround for casing issues in gohan
-    sample_ids = [id.upper() for id in sample_ids]
+    if has_filters:
+        sample_ids = biosample_id_search(phenopacket_filters=phenopacket_filters,
+                                         experiment_filters=experiment_filters, config_filters=config_filters)
+        if not sample_ids:
+            return zero_count_response()
 
     # finally, find relevant variants, depending on whether a variants query was made
     if variants_query:
+        # gohan search returns uppercase only
+        sample_ids = [id.upper() for id in sample_ids]
+
         variant_results = query_gohan(variants_query, "record", ids_only=False)
-        if phenopacket_filters or experiment_filters:
+        if has_filters:
             variant_results = list(filter(lambda v: v.get("sample_id") in sample_ids, variant_results))
         gohan_count = len(variant_results)
     else:
+        # gohan overview returns lowercase only
+        sample_ids = [id.lower() for id in sample_ids]
+
         variant_totals = gohan_totals_by_sample_id()
-        if phenopacket_filters or experiment_filters:
-            gohan_count = sum(variant_totals.get(id) for id in sample_ids)
+        if has_filters:
+            gohan_count = sum(variant_totals.get(id) for id in sample_ids if id in variant_totals)
         else:
             gohan_count = sum(variant_totals.values())
 
