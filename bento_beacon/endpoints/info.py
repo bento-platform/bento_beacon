b879e3e361093a464b77c817a8d7ac79ad9fc978
Gordon Krieger <gordon.krieger@gmail.com>
2024-02-02 18:31:53 +0000

    DATS no longer a string

--- bento_beacon/endpoints/info.py
+++ bento_beacon/endpoints/info.py
@@ -135,7 +135,7 @@ def build_service_details():
     # may be multiple datasets, so collect all descriptions into one string
     # for custom description, add a "description" field to service info in beacon_config.json
     k_datasets = katsu_datasets()
-    dats_array = list(map(lambda d: json.loads(d.get("datsFile", "{}")), k_datasets))
+    dats_array = list(map(lambda d: d.get("datsFile", {}), k_datasets))
     description = " ".join([d.get("description") for d in dats_array if "description" in d])
     if description and info.get("description") is None:
         s["description"] = description
