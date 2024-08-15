e5579f961f28129b16f478e69e806325747ecfb7
Gordon Krieger <gordon.krieger@gmail.com>
2024-08-15 15:20:09 +0000

    network utilts

--- bento_beacon/network/utils.py
+++ bento_beacon/network/utils.py
@@ -1,10 +1,16 @@
-from flask import current_app, g
 import requests
+from flask import current_app
 from urllib.parse import urlsplit, urlunsplit
 from json import JSONDecodeError
-from .network_config import BEACONS, NETWORK_INIT_TIMEOUT
-from ..utils.exceptions import APIException, InvalidQuery
-from ..utils.beacon_response import beacon_count_response
+from ..utils.exceptions import APIException
+from ..utils.katsu_utils import overview_statistics, get_katsu_config_search_fields
+from ..endpoints.info import build_service_details, overview
+from ..endpoints.biosamples import get_biosamples
+from ..endpoints.cohorts import get_cohorts
+from ..endpoints.datasets import get_datasets
+from ..endpoints.individuals import get_individuals
+from ..endpoints.variants import get_variants
+from .bento_public_query import fields_intersection, fields_union
 
 PUBLIC_SEARCH_FIELDS_PATH = "/api/metadata/api/public_search_fields"
 DEFAULT_ENDPOINT = "individuals"
@@ -13,14 +19,57 @@ OVERVIEW_STATS_QUERY = {
     "query": {"requestParameters": {}, "filters": [], "includeResultsetResponses": "ALL"},
     "bento": {"showSummaryStatistics": True},
 }
+HOST_VIEWS_BY_ENDPOINT = {
+    "biosamples": get_biosamples,
+    "cohorts": get_cohorts,
+    "datasets": get_datasets,
+    "individuals": get_individuals,
+    "variants": get_variants,
+}
 
 
+# get network node info for this beacon, which is also hosting the network
+# call methods directly instead of circular http calls
+def info_for_host_beacon():
+    service_details = build_service_details()
+
+    # TODO: fix ugly overlapping overview functions
+    # requires rolling out changes to all beacons first
+    bento_overview = overview()
+    biosample_and_experiment_stats = overview_statistics()
+    api_url = current_app.config["BEACON_BASE_URL"]
+
+    return {
+        **service_details,
+        "apiUrl": api_url,
+        "b_id": current_app.config["BEACON_ID"],
+        "overview": {
+            "individuals": {"count": bento_overview.get("counts", {}).get("individuals")},
+            "variants": bento_overview.get("variants", {}),
+            **biosample_and_experiment_stats,
+        },
+        "querySections": get_katsu_config_search_fields().get("sections", []),
+    }
+
+
+def host_beacon_response(endpoint):
+    # endpoint already known to be valid
+    return HOST_VIEWS_BY_ENDPOINT[endpoint]()
+
+
+# TODO: timeout param
 def network_beacon_call(method, url, payload=None):
+    timeout = current_app.config["NETWORK_INIT_TIMEOUT_SECONDS"]
+
+    current_app.logger.info(f"Calling network url: {url}")
+
+    # remove "verify" below, useful only for dev #############
+
     try:
         if method == "GET":
-            r = requests.get(url, timeout=NETWORK_INIT_TIMEOUT)
+            r = requests.get(url, timeout=timeout)  ##############
         else:
-            r = requests.post(url, json=payload, timeout=NETWORK_INIT_TIMEOUT)
+            r = requests.post(url, json=payload, timeout=timeout)
         beacon_response = r.json()
 
     except (requests.exceptions.RequestException, JSONDecodeError) as e:
@@ -41,11 +90,29 @@ def network_beacon_post(root_url, payload={}, endpoint=None):
     return network_beacon_call("POST", url, payload)
 
 
+def make_network_filtering_terms(beacons):
+    all_query_sections = [b["querySections"] for b in beacons.values()]
+    current_app.config["ALL_NETWORK_FILTERS"] = filters_union(all_query_sections)
+    current_app.config["COMMON_NETWORK_FILTERS"] = filters_intersection(all_query_sections)
+    pass
+
+
 def init_network_service_registry():
     current_app.logger.info("registering beacons")
+    urls = current_app.config["NETWORK_URLS"]
     network_beacons = {}
     failed_beacons = []
-    for url in BEACONS:
+    host_beacon_url = current_app.config["BEACON_BASE_URL"]
+    current_app.logger.debug(f"host url: {host_beacon_url}")
+    for url in urls:
+
+        # special handling for calling the beacon this network is hosted on
+        if url == host_beacon_url:
+            host_id = current_app.config["BEACON_ID"]
+            network_beacons[host_id] = info_for_host_beacon()
+            continue
+
+        # all other beacons
         try:
             b = network_beacon_get(url, endpoint="overview")
             beacon_info = b.get("response")
@@ -81,7 +148,7 @@ def init_network_service_registry():
         network_beacons[b_id] = beacon_info
         network_beacons[b_id]["overview"] = overview
 
-        # TODO, katsu calls are inconsistent here (v15 katsu does not respond)
+        # Note: v15 katsu does not respond here
         # TODO (longer): serve beacon spec filtering terms instead of bento public querySections
         network_beacons[b_id]["querySections"] = get_public_search_fields(url).get("sections", [])  # temp
 
@@ -95,71 +162,17 @@ def init_network_service_registry():
             f"{len(failed_beacons)} network beacon{'' if len(failed_beacons) == 1 else 's'} failed to respond: {', '.join(failed_beacons)}"
         )
 
+    make_network_filtering_terms(network_beacons)
     current_app.config["NETWORK_BEACONS"] = network_beacons
 
 
-def beacon_network_response(beacon_responses):
-    num_total_results, bento_stats = sum_network_responses(beacon_responses)
-    g.response_info["bento"] = bento_stats
-    g.response_info["network"] = beacon_responses
-
-    # beacon network hardcoded to counts
-    # to handle all possible granularities, call build_query_response() instead
-    return beacon_count_response(num_total_results)
-
-
-def sum_network_responses(beacon_responses):
-    num_total_results = 0
-    # could parameterize response, currently hardcoded in bento_public
-    experiments_count = 0
-    biosamples_count = 0
-    sampled_tissue_chart = []
-    experiment_type_chart = []
-
-    for response in beacon_responses.values():
-        num_total_results += response.get("responseSummary", {}).get("numTotalResults", 0)
-        stats = response.get("info", {}).get("bento", {})
-        experiments_count += stats.get("experiments", {}).get("count")
-        biosamples_count += stats.get("biosamples", {}).get("count")
-        sampled_tissue_chart = merge_charts(sampled_tissue_chart, stats.get("biosamples", {}).get("sampled_tissue", []))
-        experiment_type_chart = merge_charts(
-            experiment_type_chart, stats.get("experiments", {}).get("experiment_type", [])
-        )
-
-    bento_stats = {
-        "biosamples": {"count": biosamples_count, "sampled_tissue": sampled_tissue_chart},
-        "experiments": {"count": experiments_count, "experiment_type": experiment_type_chart},
-    }
-
-    return num_total_results, bento_stats
-
-
-def chart_to_dict(chart):
-    return {item["label"]: item["value"] for item in chart}
-
-
-def dict_to_chart(d):
-    return [{"label": label, "value": value} for label, value in d.items()]
-
-
-def merge_charts(c1, c2):
-    """
-    combine data from two categorical charts
-    any categories with identical names are merged into a single field with the sum of their values
-    """
-    merged = chart_to_dict(c1)
-    for cat in c2:
-        label = cat["label"]
-        value = cat["value"]
-        merged[label] = merged.get(label, 0) + value
-
-    return dict_to_chart(merged)
+##########################################
+# Temp utils for bento public search terms
 
 
 def get_public_search_fields(beacon_url):
     fields_url = public_search_fields_url(beacon_url)
     current_app.logger.info(f"trying public fields url {fields_url}")
-
     fields = network_beacon_get(fields_url)
     return fields
 
@@ -169,9 +182,9 @@ def public_search_fields_url(beacon_url):
     return urlunsplit((split_url.scheme, "portal." + split_url.netloc, PUBLIC_SEARCH_FIELDS_PATH, "", ""))
 
 
-def filtersUnion():
-    pass
+def filters_union(all_search_fields):
+    return [{"section_title": "All Filters", "fields": fields_union(all_search_fields)}]
 
 
-def filtersIntersection():
-    pass
+def filters_intersection(all_search_fields):
+    return [{"section_title": "Common Filters", "fields": fields_intersection(all_search_fields)}]
