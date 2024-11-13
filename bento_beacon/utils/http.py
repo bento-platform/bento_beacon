cf9e5efe8768929699f13329164dcbec251ff638
Gordon Krieger <gordon.krieger@gmail.com>
2024-11-13 16:25:39 +0000

    patch aiohttp params

--- bento_beacon/utils/http.py
+++ bento_beacon/utils/http.py
@@ -5,3 +5,8 @@ __all__ = ["tcp_connector"]
 
 def tcp_connector(c):
     return aiohttp.TCPConnector(verify_ssl=not c["BENTO_DEBUG"])
+
+
+# aiohttp refuses to encode bools
+def aiohttp_params(p):
+    return {k: (str(v) if isinstance(v, bool) else v) for k, v in p.items()}
