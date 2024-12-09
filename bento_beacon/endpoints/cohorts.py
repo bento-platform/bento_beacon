a6f96dba2e97ccd884456180a6291c4f7c6f9ed8
Gordon Krieger <gordon.krieger@gmail.com>
2024-12-09 18:43:57 +0000

    scope /cohorts and /datasets

--- bento_beacon/endpoints/cohorts.py
+++ bento_beacon/endpoints/cohorts.py
@@ -1,18 +1,20 @@
 from flask import Blueprint, current_app
 from ..authz.middleware import authz_middleware
 from ..utils.beacon_response import beacon_collections_response
+from ..utils.scope import scoped_route_decorator_for_blueprint
 
 cohorts = Blueprint("cohorts", __name__)
+route_with_optional_project_id = scoped_route_decorator_for_blueprint(cohorts)
 
 
-@cohorts.route("/cohorts", methods=["GET", "POST"])
+@route_with_optional_project_id("/cohorts", methods=["GET", "POST"])
 @authz_middleware.deco_public_endpoint
 def get_cohorts():
     cohorts = current_app.config["BEACON_COHORT"]
     return beacon_collections_response({"collections": cohorts})
 
 
-@cohorts.route("/cohorts/<id>", methods=["GET", "POST"])
+@route_with_optional_project_id("/cohorts/<id>", methods=["GET", "POST"])
 @authz_middleware.deco_public_endpoint
 def get_cohort_by_id(id):
     cohorts = current_app.config["BEACON_COHORT"]
--- bento_beacon/endpoints/datasets.py
+++ bento_beacon/endpoints/datasets.py
@@ -3,10 +3,10 @@ from ..authz.middleware import authz_middleware
 from ..utils.beacon_response import beacon_collections_response
 from ..utils.katsu_utils import katsu_datasets, katsu_dataset_by_id
 from ..utils.beacon_mappings import katsu_to_beacon_dataset_mapping
-from ..utils.scope import scoped_route_decorator_generator
+from ..utils.scope import scoped_route_decorator_for_blueprint
 
 datasets = Blueprint("datasets", __name__)
-route_with_optional_project_id = scoped_route_decorator_generator(datasets)
+route_with_optional_project_id = scoped_route_decorator_for_blueprint(datasets)
 
 
 @route_with_optional_project_id("/datasets", methods=["GET", "POST"])
