abeaf9e5272936b9af64c2755769a13b728ee9fb
Gordon Krieger <gordon.krieger@gmail.com>
2025-07-29 19:34:52 +0000

    instantiate network objects first, then do async calls

--- bento_beacon/network/network_beacons.py
+++ bento_beacon/network/network_beacons.py
@@ -1,10 +1,10 @@
 import asyncio
 import aiohttp
-from abc import ABC, abstractmethod
-from flask import current_app
 from json import JSONDecodeError
-from packaging.version import Version
-from urllib.parse import urljoin
+from abc import ABC, abstractmethod
+from copy import deepcopy
+from flask import current_app
+from ..utils.http import tcp_connector
 from ..utils.exceptions import APIException
 from ..endpoints.biosamples import get_biosamples
 from ..endpoints.cohorts import get_cohorts
@@ -16,9 +16,9 @@ from ..endpoints.info_scoped import (
     get_filtering_terms as local_beacon_filtering_terms,
     overview as local_beacon_overview,
 )
-from ..utils.http import tcp_connector
 
-OVERVIEW_ENDPOINT = "/overview"
+OVERVIEW_ENDPOINT = "overview"
+FILTERING_TERMS_ENDPOINT = "filtering_terms"
 
 HOST_VIEWS_BY_ENDPOINT = {
     "biosamples": get_biosamples,
@@ -29,149 +29,88 @@ HOST_VIEWS_BY_ENDPOINT = {
 }
 
 
-async def new_info_for_host_beacon():
-    return {"info for host": "beacon"}
-
-
-# class for network possibly overkill
-# only functionality we need is:
-#  - list of beacons / list of filtering terms
-#  - query a beacon
-#  - we don't even need to query the whole network, since that's done in the UI client
-
-
-class NetworkBeacon(ABC):
+class NetworkNode(ABC):
 
     # shared constructor for subclasses
-    def __init__(self, beacon_name, api_url, service_details, overview, filtering_terms):
-        current_app.logger.info(f"init beacon {beacon_name}")
-        self.beacon_name: str = beacon_name
-        self.api_url: str = api_url
-        self.service_details: dict = service_details  # "service_details"? not "service_info"?
-        self.bento_beacon_version = service_details.get("version")
-        # required fields in service details:
-        # - id
-        # - name
-        # - apiVersion
-        # - environment
-        # - organization (nested obj)
+    def __init__(self, config):
+        current_app.logger.info(f"init beacon {config.get('name')}")
+        self.config = config  # needed? we can just pull what we need / partition into useful parts
+        self.api_url = config.get("api_url")
+        self.id = config.get("id")
 
-        self.overview: dict = overview
-        self.filtering_terms: list = filtering_terms
-        # more params
+        # filled in later
+        self.service_details = None
+        self.overview = None
+        self.filtering_terms = None
 
-    # factory method for async init
-    @classmethod
-    @abstractmethod
-    async def init_beacon(cls, beacon_name, api_url):
-        pass
+        # self.bento_beacon_version = service_details.get("version")
 
     @abstractmethod
-    async def get_network_beacon_info(self):
-        pass
-
-    @abstractmethod
-    async def get_overview(self):
-        pass
-
-    @abstractmethod
-    async def get_filtering_terms(self):
+    async def retrieve_beacon_info(self):
         pass
 
     @abstractmethod
     async def query_beacon(self):
         pass
 
+    def node_info_to_json(self):
+        # filtering terms?
+        return {**self.service_details, "apiUrl": self.api_url}
 
-# all responses should be **without** the beacon response wrapper, since this is added at the end
 
+class HostBeacon(NetworkNode):
 
-class HostNode(NetworkBeacon):
-    """
-    Class for the local beacon that is also hosting the network.
-    Network hosts are not required to have beacons of their own, although this is the typical use case.
-    For this local beacon we call python methods directly instead of making circular http calls.
-    """
-
-    @classmethod
-    async def init_beacon(cls, beacon_name, api_url):
-        current_app.logger.info("init host beacon")
-
-        # call python methods instead of circular http calls
-        service_details = await local_beacon_format_service_details()
-        overview = await local_beacon_overview()  # has response wrapper
-        filtering_terms = await local_beacon_filtering_terms(project_id=None, dataset_id=None)
-        return cls(beacon_name, api_url, service_details, overview, filtering_terms)
-
-    async def get_network_beacon_info(self):
-        # get overview & filtering_terms here, save in obj & return
-        # what is this method for? populating values?
-        return "host info"
-
-    async def get_overview(self):
-        return 0
-
-    async def get_filtering_terms(self):
-        return 0
+    async def retrieve_beacon_info(self):
+        # gather? most of this code isn't really async
+        self.service_details = await local_beacon_format_service_details()
+        self.overview = (await local_beacon_overview()).get("overview", {})
+        self.filtering_terms = await local_beacon_filtering_terms(project_id=None, dataset_id=None)
 
     async def query_beacon(self):
-        return 0
+        return {}
 
 
-class BentoNetworkNode(NetworkBeacon):
-    """
-    Class for a bento beacon in the network other than the host beacon
-    """
+class NetworkBeacon(NetworkNode):
 
-    @classmethod
-    async def init_beacon(cls, beacon_name, api_url):
-        current_app.logger.info(f"init network beacon {beacon_name}")
-        timeout = current_app.config["NETWORK_DEFAULT_TIMEOUT_SECONDS"]
-        overview_url = api_url + OVERVIEW_ENDPOINT
-
-        service_details_and_overview = (await BentoNetworkNode._network_beacon_call("GET", overview_url, timeout)).get(
-            "response", {}
-        )
-        service_details = {k: v for k, v in service_details_and_overview.items() if k != "overview"}
-        overview = service_details_and_overview.get("response", {}).get("overview", {})
-
+    async def retrieve_beacon_info(self):
+        # two network calls, but both are to the same beacon
+        overview_response = (await self._network_beacon_get(OVERVIEW_ENDPOINT)).get("response")
+        service_details = {k: v for k, v in overview_response.items() if k != "overview"}
+        overview = overview_response.get("overview", {})
+        # does not handle API error
         filtering_terms = (
-            (await BentoNetworkNode._network_beacon_call("GET", overview_url, timeout))
-            .get("response", {})
-            .get("filteringTerms", [])
+            (await self._network_beacon_get(FILTERING_TERMS_ENDPOINT)).get("response", {}).get("filteringTerms", [])
         )
 
-        return cls(beacon_name, api_url, service_details, overview, filtering_terms)
+        # if there are versioning changes necessary for this beacon, apply them here (or on the fly?)
+        # then save
+        self.service_details = service_details
+        self.overview = overview
+        self.filtering_terms = filtering_terms
 
-    # needs versioning
-    async def get_overview(self):
-        overview = await self._network_beacon_get("/overview").get("response")
-        return overview
-
-    # needs versioning
-    async def get_filtering_terms(self):
-        filters = await self._network_beacon_get("/filtering_terms").get("response")
-        return 0
-
-    # needs versioning
     async def query_beacon(self, payload, endpoint):
-        return await self._network_beacon_post(payload, endpoint)
+        # apply any versioning to request payload
+        results = await self._network_beacon_post(payload, endpoint)
+        # apply any versioning to response format
+        return results
 
-    # class method since we need to make api calls before calling the constructor
-    @classmethod
-    async def _network_beacon_call(cls, method, url, timeout, payload=None):
+    def request_timeout(self, payload=None):
+        return (
+            current_app.config["NETWORK_VARIANTS_QUERY_TIMEOUT_SECONDS"]
+            if self.has_variants_query(payload)
+            else current_app.config["NETWORK_DEFAULT_TIMEOUT_SECONDS"]
+        )
 
+    async def _network_beacon_call(self, method, url, timeout, payload=None):
         current_app.logger.info(f"Calling network url: {url}")
-
         try:
             async with aiohttp.ClientSession(connector=tcp_connector(current_app.config)) as s:
                 async with (
                     s.get(url, timeout=timeout) if method == "GET" else s.post(url, timeout=timeout, json=payload)
                 ) as r:
-
                     if not r.ok:
+                        current_app.logger.error("failed network call to {url}")
                         raise APIException()
-
                     beacon_response = await r.json()
 
         except (APIException, aiohttp.ClientError, JSONDecodeError) as e:
@@ -180,7 +119,6 @@ class BentoNetworkNode(NetworkBeacon):
 
         return beacon_response
 
-    # currently we don't do queries by GET, can be added when we take on external beacons
     async def _network_beacon_get(self, endpoint=None):
         url = self.api_url if endpoint is None else self.api_url + "/" + endpoint
         timeout = current_app.config["NETWORK_DEFAULT_TIMEOUT_SECONDS"]
@@ -191,103 +129,142 @@ class BentoNetworkNode(NetworkBeacon):
         timeout = self.request_timeout(payload)
         return await self._network_beacon_call("POST", url, timeout, payload)
 
-    def request_timeout(self, payload=None):
-        return (
-            current_app.config["NETWORK_VARIANTS_QUERY_TIMEOUT_SECONDS"]
-            if self.has_variants_query(payload)
-            else current_app.config["NETWORK_DEFAULT_TIMEOUT_SECONDS"]
-        )
 
-    @staticmethod
-    def has_variants_query(payload):
-        if not payload:
-            return False
-        query = payload.get("requestParameters", {}).get("g_variant")
-        return bool(query)
-
-
-# class ExternalNetworkNode(NetworkBeacon):
-#     pass
-
-# Guidelines for adding a non-Bento beacon to the network
-#
-# Stuff to add to config (config details you will need that are somehow not discoverable):
-# - "query_verb" indicating whether this beacon takes queries by GET or POST
-#
-# - "assemblies" showing which assemblies this beacon contains
-#
-# - any remapping needed for the request or the response
-# - - may need to map between alphanumeric filters and corresponding ontologies
-# - - there may be relevant response details in the "info" field, which is a free field in the spec
-#
-# - any of the various beacon spec choices that are available but hard to discover
-# - - "do I use filtering terms from /filtering_terms or from individuals/filtering_terms?"
-#
-# - any idiosyncracies that deviate from the spec
-# - - some beacons may have custom request urls not matching the ones expected in the spec
-# - - response format may deviate from the spec for whatever reason
-#
-# for bento beacons, we can get both "apiVersion" (version of beacon spec) and "version" (version of this implementation)
-# from '/overview", which follows the spec for the root endpoint "/"
-# But in the spec for /, BOTH "apiVersion" and "version" point to the same thing (the beacon spec version)
-# This is an oversight in the spec.
-# But both should be available from /service-info, which correctly distinguishes between apiVersion (type.version) and implementation version (version)
-# But /service-info is very limited and may be missing details such as logo_url. So external beacons may need multiple calls to get all service details
-
-
-# adapt queries from the network accordingly
-# ... this may require lots of mapping, eg between alphanumeric values and corresponding ontologies
+# -----------------------------------
 
 
 async def new_init_network_service_registry():
     config = current_app.config["NEW_NETWORK_CONFIG"]
-    network = config.get("network")  # beacons in this network
-    beacons = config.get("beacons")  # info for each beacon
+    network_beacons = config.get("beacons")
     host_beacon_url = current_app.config["BEACON_BASE_URL"]
 
-    network_beacons_dict: dict[str, NetworkBeacon] = {}
-    async_init_calls = []
-
-    for beacon_name in network:
-        api_url = beacons.get(beacon_name, {}).get("api_url")
-        if api_url == host_beacon_url:
-            async_init_calls.append(HostNode.init_beacon(beacon_name, api_url))
+    # instantiate skeleton beacon objects
+    beacons = []
+    for beacon_config in network_beacons.values():
+        if beacon_config.get("api_url") == host_beacon_url:
+            beacons.append(HostBeacon(beacon_config))
         else:
-            async_init_calls.append(BentoNetworkNode.init_beacon(beacon_name, api_url))
+            beacons.append(NetworkBeacon(beacon_config))
 
-    current_app.logger.info("making calls")
+    # fill in service details and filtering terms for each beacon
+    beacon_calls = []
+    for b in beacons:
+        beacon_calls.append(b.retrieve_beacon_info())
+    await asyncio.gather(*beacon_calls, return_exceptions=False)
 
-    results = await asyncio.gather(*async_init_calls, return_exceptions=True)
+    # retrieve_beacon_info() returns None... perhaps could return True on success
+    # don't throw away beacons that fail to respond... add handling for these
+    # we can try them again later
+    # also these errors are logged elsewhere so don't bother logging them here
 
-    current_app.logger.info(results)
+    # create network-wide filtering terms
+    # do we need to save these?
+    network_filtering_terms = await get_network_filtering_terms(beacons)
 
-    # filter out any failed calls
-    registered_beacons = [b for b in results if not isinstance(b, Exception)]
+    # collect network assemblies
+    # not strictly necessary since client can already get this from response
+    # network_reference_assemblies = get_network_reference_assemblies(beacons)
 
-    current_app.logger.info(f"registered {len(registered_beacons)} beacons in network")
+    # save beacons as a dict by id
+    # alternatively, make a Network class and save there instead
+    current_app.config["NETWORK_BEACONS"] = {b.id: b for b in beacons}
 
-    return network_beacons_dict
+    results = {
+        "beacons": [b.node_info_to_json() for b in beacons],
+        "filtersIntersection": network_filtering_terms.get("filtersIntersection"),
+        "filtersUnion": network_filtering_terms.get("filtersUnion"),
+        # "referenceAssemblies": network_reference_assemblies,
+    }
 
-    # want {"bqc19": <bqc19 beacon obj>, "local": }
+    return results
 
-    # step 1: create objects for each beacon (this makes obj with url only, no api calls)
 
-    # step 2: init each beacon using asyncio.gather
+async def get_network_filtering_terms(beacons: list[NetworkNode]):
+    all_filtering_terms = [b.filtering_terms for b in beacons if b.filtering_terms is not None]
+    num_beacons_answering = len(all_filtering_terms)
 
-    # list of function calls, not http calls
-    # but should probably make this a list of objects, not methods
-    # : list[NetworkBeacon] = []
+    filters_dict = get_filters_dict(flatten(all_filtering_terms))
+    return {
+        "filtersUnion": get_union_of_filtering_terms(filters_dict),
+        "filtersIntersection": get_intersection_of_filtering_terms(filters_dict, num_beacons_answering),
+    }
 
-    # get dict of beacons from config
-    # init object for each beacon
-    # call beacons for filtering terms, not katsu (this is a change from old method)
 
-    # for bento beacons we can call /overview and get everything we need
-    # for non-bento beacons:
-    # - call / or /service-info for some stuff, and
-    # other stuff can be filled in from config, like
-    # assemblies, query verb, or anything that should be in / but is missing, like logourl
+# -------------------------------
+# filter utils
+# could probably go in a different file
 
-    # generate network filtering terms
-    # ideally do this from beacons instead of katsu, so format may change
-    # old code calls this: make_network_filtering_terms(beacon_dict)
+
+def flatten(nested_list):
+    return [item for nested_items in nested_list for item in nested_items]
+
+
+def get_union_of_filtering_terms(filters_dict):
+    # create an entry for each filter
+    union_filters = []
+    for f in filters_dict.values():
+        filter = deepcopy(f[0])  # arbitrarily get name, description, etc from first entry
+        filter["values"] = values_union([entry["values"] for entry in f])
+        union_filters.append(filter)
+
+    return union_filters
+
+
+def get_intersection_of_filtering_terms(filters_dict, num_beacons_in_network):
+    # remove any fields not in all entries
+    intersection_dict = {id: entries for id, entries in filters_dict.items() if len(entries) == num_beacons_in_network}
+
+    # create one entry for each id
+    intersection_filters = []
+    for f in intersection_dict.values():
+        filter = deepcopy(f[0])  # arbitrarily get name, description, etc from first entry
+        values = values_intersection([entry["values"] for entry in f])
+        if values:
+            filter["values"] = values
+            intersection_filters.append(filter)
+
+    return intersection_filters
+
+
+def get_filters_dict(filters_list):
+    # make a dict of entries, keyed to bento query id, keeping duplicates in an array
+    # TODO: change key to model mapping (phenopackets / experiments path) instead of bento id
+    filters_by_id = {}
+    for f in filters_list:
+        filter_id = f["id"]
+        filters_by_id[filter_id] = filters_by_id.get(filter_id, []) + [f]
+    return filters_by_id
+
+
+def values_union(options_list):
+    # remove duplicates but keep any ordering
+    return list(dict.fromkeys(flatten(options_list[:])))
+
+
+def values_intersection(options_list):
+    num_instances = len(options_list)
+    flat_options = flatten(options_list[:])
+    # only keep options that are present in all instances, preserving order
+    counter = {}
+    for option in flat_options:
+        counter[option] = counter.get(option, 0) + 1
+
+    intersection = [key for key in counter if counter[key] == num_instances]
+    return intersection
+
+
+# -----------------------
+# reference utils
+
+# TODO later:
+# links for gene name lookups for each assembly (some may not exist in local beacon)
+# note that for network queries, we only need gene *names*, position information is looked up locally on each beacon.
+
+
+# needed? can already be inferred by client
+def get_network_reference_assemblies(beacons: list[NetworkNode]):
+    network_assemblies = set()
+    for b in beacons:
+        assemblies = list(b.overview.get("variants", {}).keys())
+        network_assemblies.update(assemblies)
+    return sorted(list(network_assemblies))
