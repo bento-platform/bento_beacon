a0e903a4b2594afd1b882fd7013e568c66af3099
Gordon Krieger <gordon.krieger@gmail.com>
2025-07-31 19:48:09 +0000

    add some network tests

--- tests/test_beacon_network.py
+++ tests/test_beacon_network.py
@@ -11,12 +11,14 @@ from .test_routes import (
     mock_katsu_private_search_overview,
     mock_gohan_query,
     KATSU_QUERY_PARAMS,
+    BEACON_REQUEST_BODY,
 )
 from .data.service_responses import (
     network_beacon_overview_bento_18,
     network_beacon_query_response_bento_18,
-    network_beacon_filtering_terms_response_bento_18
+    network_beacon_filtering_terms_response_bento_18,
 )
+
 
 def mock_network_beacon_overview(aioresponse):
     url = "https://fake2.bento.ca/api/beacon/overview"
@@ -30,16 +32,15 @@ def mock_network_beacon_query_response(aioresponse):
 
 def mock_network_beacon_filtering_terms(aioresponse):
     url = "https://fake2.bento.ca/api/beacon/filtering_terms"
-    aioresponse.post(url, payload=network_beacon_filtering_terms_response_bento_18)
+    aioresponse.get(url, payload=network_beacon_filtering_terms_response_bento_18)
 
 
-def test_network_endpoint(app_config, client, aioresponse):
+def mock_network_init(app_config, aioresponse):
     mock_permissions_all(app_config, aioresponse)
 
     # calls to network beacons
     mock_network_beacon_overview(aioresponse)
     mock_network_beacon_filtering_terms(aioresponse)
-    mock_network_beacon_query_response(aioresponse)
 
     # calls for local beacon hosting the network
     mock_katsu_public_rules(app_config, aioresponse)
@@ -53,6 +54,32 @@ def test_network_endpoint(app_config, client, aioresponse):
     mock_katsu_private_search_overview(app_config, aioresponse)
     mock_gohan_query(app_config, aioresponse)
 
+
+def test_network_endpoint(app_config, client, aioresponse):
+    mock_permissions_all(app_config, aioresponse)
+    mock_network_init(app_config, aioresponse)
     response = client.get("/network")
     assert response.status_code == 200
     assert "beacons" in response.get_json()
+
+
+def test_network_config_missing(app_config, client):
+    app_config["NETWORK_CONFIG"] = {}
+    response = client.get("/network")
+    assert response.status_code == 500
+
+
+def test_network_query_bad_endpoint(app_config, client, aioresponse):
+    mock_permissions_all(app_config, aioresponse)
+    mock_network_init(app_config, aioresponse)
+    mock_network_beacon_query_response(aioresponse)
+    INCORRECT_ENDPOINT = "individualzz"
+    response = client.post(f"/network/beacons/ca.fake2.bento.beacon/{INCORRECT_ENDPOINT}", json=BEACON_REQUEST_BODY)
+    assert response.status_code == 404
+
+
+def test_network_query_bad_http_verb(app_config, client, aioresponse):
+    mock_permissions_all(app_config, aioresponse)
+    mock_network_init(app_config, aioresponse)
+    response = client.get(f"/network/beacons/ca.fake2.bento.beacon/individuals")
+    assert response.status_code == 500
